1a1919e161cabdafc768d805f9e795a350af8fb2
Paul Gesel <paulgesel@gmail.com>
2022-08-08 08:25:33 -0600

    Update kinematics_interface_kdl/src/kinematics_interface_kdl.cpp
    Co-authored-by: AndyZe <andyz@utexas.edu>

--- kinematics_interface_kdl/src/kinematics_interface_kdl.cpp
+++ kinematics_interface_kdl/src/kinematics_interface_kdl.cpp
@@ -186,7 +186,7 @@ bool KDLKinematics::verify_link_name(const std::string & link_name)
   if (link_name_map_.find(link_name) == link_name_map_.end())
   {
     std::string links;
-    for (auto i = 0u; i < chain_.getNrOfSegments(); i++)
+    for (auto i = 0u; i < chain_.getNrOfSegments(); ++i)
     {
       links += "\n" + chain_.getSegment(i).getName();
     }
