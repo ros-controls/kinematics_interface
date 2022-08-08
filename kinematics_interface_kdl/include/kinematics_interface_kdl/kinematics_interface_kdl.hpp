902f8adea595e53e0b626f75f554741940ed8c99
Paul Gesel <paul.gesel@picknik.ai>
2022-08-08 13:58:07 -0600

    add pre-commit to repo (#7)
    add pre-commit

--- kinematics_interface_kdl/include/kinematics_interface_kdl/kinematics_interface_kdl.hpp
+++ kinematics_interface_kdl/include/kinematics_interface_kdl/kinematics_interface_kdl.hpp
@@ -17,30 +17,29 @@
 
 #pragma once
 
-#include "kinematics_interface/kinematics_interface_base.hpp"
 #include <rclcpp_lifecycle/lifecycle_node.hpp>
-#include "kdl_parser/kdl_parser.hpp"
+#include "eigen3/Eigen/Core"
+#include "eigen3/Eigen/LU"
 #include "kdl/chainfksolverpos_recursive.hpp"
 #include "kdl/chainfksolvervel_recursive.hpp"
 #include "kdl/chainjnttojacsolver.hpp"
 #include "kdl/treejnttojacsolver.hpp"
-#include "eigen3/Eigen/Core"
-#include "eigen3/Eigen/LU"
-
+#include "kdl_parser/kdl_parser.hpp"
+#include "kinematics_interface/kinematics_interface_base.hpp"
 
 namespace kinematics_interface_kdl
 {
-
-    class KDLKinematics : public kinematics_interface::KinematicsBaseClass
-    {
-    public:
-        /**
+class KDLKinematics : public kinematics_interface::KinematicsBaseClass
+{
+public:
+  /**
          * \brief KDL implementation of ros2_control kinematics interface
          */
-        bool
-        initialize(std::shared_ptr<rclcpp_lifecycle::LifecycleNode> node, const std::string &end_effector_name) override;
+  bool initialize(
+    std::shared_ptr<rclcpp_lifecycle::LifecycleNode> node,
+    const std::string & end_effector_name) override;
 
-      /**
+  /**
        * \brief Convert Cartesian delta-x to joint delta-theta, using the Jacobian.
        * \param[in] joint_pos joint positions of the robot in radians
        * \param[in] delta_x_vec input Cartesian deltas (x, y, z, wx, wy, wz)
@@ -48,13 +47,11 @@ namespace kinematics_interface_kdl
        * \param[out] delta_theta_vec output vector with joint states
        * \return true if successful
        */
-      bool
-      convert_cartesian_deltas_to_joint_deltas(const std::vector<double> &joint_pos,
-                                               const std::vector<double> &delta_x_vec,
-                                               const std::string &link_name,
-                                               std::vector<double> &delta_theta_vec) override;
+  bool convert_cartesian_deltas_to_joint_deltas(
+    const std::vector<double> & joint_pos, const std::vector<double> & delta_x_vec,
+    const std::string & link_name, std::vector<double> & delta_theta_vec) override;
 
-      /**
+  /**
        * \brief Convert joint delta-theta to Cartesian delta-x.
        * \param joint_pos joint positions of the robot in radians
        * \param[in] delta_theta_vec vector with joint states
@@ -62,61 +59,58 @@ namespace kinematics_interface_kdl
        * \param[out] delta_x_vec  Cartesian deltas (x, y, z, wx, wy, wz)
        * \return true if successful
        */
-      bool
-      convert_joint_deltas_to_cartesian_deltas(const std::vector<double> &joint_pos,
-                                               const std::vector<double> &delta_theta_vec,
-                                               const std::string &link_name,
-                                               std::vector<double> &delta_x_vec) override;
+  bool convert_joint_deltas_to_cartesian_deltas(
+    const std::vector<double> & joint_pos, const std::vector<double> & delta_theta_vec,
+    const std::string & link_name, std::vector<double> & delta_x_vec) override;
 
-      /**
+  /**
       * \brief Calculates the joint transform for a specified link using provided joint positions.
       * \param[in] joint_pos joint positions of the robot in radians
       * \param[in] link_name the name of the link to find the transform for
       * \param[out] transform_vec transformation matrix of the specified link in column major format.
       * \return true if successful
       */
-      bool
-      calculate_link_transform(const std::vector<double> &joint_pos, const std::string &link_name,
-                               std::vector<double> &transform_vec) override;
+  bool calculate_link_transform(
+    const std::vector<double> & joint_pos, const std::string & link_name,
+    std::vector<double> & transform_vec) override;
 
-      /**
+  /**
       * \brief Calculates the joint transform for a specified link using provided joint positions.
       * \param[in] joint_pos joint positions of the robot in radians
       * \param[in] link_name the name of the link to find the transform for
       * \param[out] jacobian Jacobian matrix of the specified link in column major format.
       * \return true if successful
       */
-      bool
-      calculate_jacobian(const std::vector<double> &joint_pos, const std::string &link_name,
-                         std::vector<double> &jacobian) override;
+  bool calculate_jacobian(
+    const std::vector<double> & joint_pos, const std::string & link_name,
+    std::vector<double> & jacobian) override;
 
+private:
+  bool update_joint_array(const std::vector<double> & joint_pos);
 
-    private:
-        bool update_joint_array(const std::vector<double>& joint_pos);
+  //verification methods
+  bool verify_initialized();
+  bool verify_link_name(const std::string & link_name);
+  bool verify_transform_vector(const std::vector<double> & transform);
+  bool verify_cartesian_vector(const std::vector<double> & cartesian_vector);
+  bool verify_joint_vector(const std::vector<double> & joint_vector);
+  bool verify_jacobian(const std::vector<double> & jacobian_vector);
 
-        //verification methods
-        bool verify_initialized();
-        bool verify_link_name(const std::string& link_name);
-        bool verify_transform_vector(const std::vector<double> &transform);
-        bool verify_cartesian_vector(const std::vector<double> &cartesian_vector);
-        bool verify_joint_vector(const std::vector<double> &joint_vector);
-        bool verify_jacobian(const std::vector<double> &jacobian_vector);
-
-        bool initialized = false;
-        std::string root_name_;
-        size_t num_joints_;
-        KDL::Chain chain_;
-        std::shared_ptr<KDL::ChainFkSolverPos_recursive> fk_pos_solver_;
-        KDL::JntArray q_;
-        KDL::Frame frame_;
-        std::shared_ptr<KDL::Jacobian> jacobian_;
-        std::shared_ptr<KDL::ChainJntToJacSolver> jac_solver_;
-        std::shared_ptr<rclcpp_lifecycle::LifecycleNode> node_;
-        std::unordered_map<std::string, int> link_name_map_;
-        double alpha; // damping term for Jacobian inverse
-        Eigen::Matrix<double,6,1> delta_x;
-        Eigen::VectorXd delta_theta;
-        Eigen::MatrixXd I;
-    };
+  bool initialized = false;
+  std::string root_name_;
+  size_t num_joints_;
+  KDL::Chain chain_;
+  std::shared_ptr<KDL::ChainFkSolverPos_recursive> fk_pos_solver_;
+  KDL::JntArray q_;
+  KDL::Frame frame_;
+  std::shared_ptr<KDL::Jacobian> jacobian_;
+  std::shared_ptr<KDL::ChainJntToJacSolver> jac_solver_;
+  std::shared_ptr<rclcpp_lifecycle::LifecycleNode> node_;
+  std::unordered_map<std::string, int> link_name_map_;
+  double alpha;  // damping term for Jacobian inverse
+  Eigen::Matrix<double, 6, 1> delta_x;
+  Eigen::VectorXd delta_theta;
+  Eigen::MatrixXd I;
+};
 
 }  // namespace kinematics_interface_kdl
