326c7bb2307fa175805b119c134dfc28a0bbb925
Denis Štogl <denis@stogl.de>
2022-08-17 22:40:22 +0200

    remove duplicated documentation

--- kinematics_interface_kdl/include/kinematics_interface_kdl/kinematics_interface_kdl.hpp
+++ kinematics_interface_kdl/include/kinematics_interface_kdl/kinematics_interface_kdl.hpp
@@ -39,55 +39,22 @@ namespace kinematics_interface_kdl
 class KinematicsInterfaceKDL : public kinematics_interface::KinematicsInterface
 {
 public:
-  /**
-     * \brief Initialize plugin. This method must be called before any other.
-     */
   bool initialize(
     std::shared_ptr<rclcpp::node_interfaces::NodeParametersInterface> parameters_interface,
     const std::string & end_effector_name) override;
 
-  /**
-     * \brief Convert Cartesian delta-x to joint delta-theta, using the Jacobian.
-     * \param[in] joint_pos joint positions of the robot in radians
-     * \param[in] delta_x input Cartesian deltas (x, y, z, wx, wy, wz)
-     * \param[in] link_name the link name at which delta_x is applied
-     * \param[out] delta_theta outputs joint deltas
-     * \return true if successful
-     */
   bool convert_cartesian_deltas_to_joint_deltas(
     const Eigen::VectorXd & joint_pos, const Eigen::Matrix<double, 6, 1> & delta_x,
     const std::string & link_name, Eigen::VectorXd & delta_theta) override;
 
-  /**
-     * \brief Convert joint delta-theta to Cartesian delta-x.
-     * \param joint_pos joint positions of the robot in radians
-     * \param[in] delta_theta joint deltas
-     * \param[in] link_name the link name at which delta_x is calculated
-     * \param[out] delta_x  Cartesian deltas (x, y, z, wx, wy, wz)
-     * \return true if successful
-     */
   bool convert_joint_deltas_to_cartesian_deltas(
     const Eigen::VectorXd & joint_pos, const Eigen::VectorXd & delta_theta,
     const std::string & link_name, Eigen::Matrix<double, 6, 1> & delta_x) override;
 
-  /**
-    * \brief Calculates the joint transform for a specified link using provided joint positions.
-    * \param[in] joint_pos joint positions of the robot in radians
-    * \param[in] link_name the name of the link to find the transform for
-    * \param[out] transform transformation matrix of the specified link
-    * \return true if successful
-    */
   bool calculate_link_transform(
     const Eigen::VectorXd & joint_pos, const std::string & link_name,
     Eigen::Isometry3d & transform) override;
 
-  /**
-    * \brief Calculates the jacobian for a specified link using provided joint positions.
-    * \param[in] joint_pos joint positions of the robot in radians
-    * \param[in] link_name the name of the link to find the transform for
-    * \param[out] jacobian Jacobian matrix of the specified link in column major format.
-    * \return true if successful
-    */
   bool calculate_jacobian(
     const Eigen::VectorXd & joint_pos, const std::string & link_name,
     Eigen::Matrix<double, 6, Eigen::Dynamic> & jacobian) override;
