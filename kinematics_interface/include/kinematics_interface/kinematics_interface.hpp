f378367c91a7d5c3bcbb5ba5654cbd2f92f9f593
Denis Štogl <denis@stoglrobotics.de>
2022-08-21 18:40:47 +0200

    Make formatting of comments unified and nice.

--- kinematics_interface/include/kinematics_interface/kinematics_interface.hpp
+++ kinematics_interface/include/kinematics_interface/kinematics_interface.hpp
@@ -40,54 +40,54 @@ public:
   virtual ~KinematicsInterface() = default;
 
   /**
-     * \brief Initialize plugin. This method must be called before any other.
-     */
+   * \brief Initialize plugin. This method must be called before any other.
+   */
   virtual bool initialize(
     std::shared_ptr<rclcpp::node_interfaces::NodeParametersInterface> parameters_interface,
     const std::string & end_effector_name) = 0;
 
   /**
-     * \brief Convert Cartesian delta-x to joint delta-theta, using the Jacobian.
-     * \param[in] joint_pos joint positions of the robot in radians
-     * \param[in] delta_x input Cartesian deltas (x, y, z, wx, wy, wz)
-     * \param[in] link_name the link name at which delta_x is applied
-     * \param[out] delta_theta outputs joint deltas
-     * \return true if successful
-     */
+   * \brief Convert Cartesian delta-x to joint delta-theta, using the Jacobian.
+   * \param[in] joint_pos joint positions of the robot in radians
+   * \param[in] delta_x input Cartesian deltas (x, y, z, wx, wy, wz)
+   * \param[in] link_name the link name at which delta_x is applied
+   * \param[out] delta_theta outputs joint deltas
+   * \return true if successful
+   */
   virtual bool convert_cartesian_deltas_to_joint_deltas(
     const Eigen::VectorXd & joint_pos, const Eigen::Matrix<double, 6, 1> & delta_x,
     const std::string & link_name, Eigen::VectorXd & delta_theta) = 0;
 
   /**
-     * \brief Convert joint delta-theta to Cartesian delta-x.
-     * \param joint_pos joint positions of the robot in radians
-     * \param[in] delta_theta joint deltas
-     * \param[in] link_name the link name at which delta_x is calculated
-     * \param[out] delta_x  Cartesian deltas (x, y, z, wx, wy, wz)
-     * \return true if successful
-     */
+   * \brief Convert joint delta-theta to Cartesian delta-x.
+   * \param joint_pos joint positions of the robot in radians
+   * \param[in] delta_theta joint deltas
+   * \param[in] link_name the link name at which delta_x is calculated
+   * \param[out] delta_x  Cartesian deltas (x, y, z, wx, wy, wz)
+   * \return true if successful
+   */
   virtual bool convert_joint_deltas_to_cartesian_deltas(
     const Eigen::VectorXd & joint_pos, const Eigen::VectorXd & delta_theta,
     const std::string & link_name, Eigen::Matrix<double, 6, 1> & delta_x) = 0;
 
   /**
-    * \brief Calculates the joint transform for a specified link using provided joint positions.
-    * \param[in] joint_pos joint positions of the robot in radians
-    * \param[in] link_name the name of the link to find the transform for
-    * \param[out] transform transformation matrix of the specified link
-    * \return true if successful
-    */
+   * \brief Calculates the joint transform for a specified link using provided joint positions.
+   * \param[in] joint_pos joint positions of the robot in radians
+   * \param[in] link_name the name of the link to find the transform for
+   * \param[out] transform transformation matrix of the specified link
+   * \return true if successful
+   */
   virtual bool calculate_link_transform(
     const Eigen::VectorXd & joint_pos, const std::string & link_name,
     Eigen::Isometry3d & transform) = 0;
 
   /**
-    * \brief Calculates the jacobian for a specified link using provided joint positions.
-    * \param[in] joint_pos joint positions of the robot in radians
-    * \param[in] link_name the name of the link to find the transform for
-    * \param[out] jacobian Jacobian matrix of the specified link in column major format.
-    * \return true if successful
-    */
+   * \brief Calculates the jacobian for a specified link using provided joint positions.
+   * \param[in] joint_pos joint positions of the robot in radians
+   * \param[in] link_name the name of the link to find the transform for
+   * \param[out] jacobian Jacobian matrix of the specified link in column major format.
+   * \return true if successful
+   */
   virtual bool calculate_jacobian(
     const Eigen::VectorXd & joint_pos, const std::string & link_name,
     Eigen::Matrix<double, 6, Eigen::Dynamic> & jacobian) = 0;
