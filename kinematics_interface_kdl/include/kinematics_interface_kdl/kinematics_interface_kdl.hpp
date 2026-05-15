90ae73aca72060461acef8f302eaf343f5a76545
oguzhan.bozoglu <oguzhan.bozoglu@b-robotized.com>
2026-05-15 11:22:18 +0200

    Separate const api changes from kinematics node

--- kinematics_interface_kdl/include/kinematics_interface_kdl/kinematics_interface_kdl.hpp
+++ kinematics_interface_kdl/include/kinematics_interface_kdl/kinematics_interface_kdl.hpp
@@ -65,7 +65,7 @@ public:
     Eigen::Matrix<double, Eigen::Dynamic, 6> & jacobian_inverse) override;
 
   bool calculate_frame_difference(
-    const Eigen::Matrix<double, 7, 1> & x_a, const Eigen::Matrix<double, 7, 1> & x_b, double dt,
+    Eigen::Matrix<double, 7, 1> & x_a, Eigen::Matrix<double, 7, 1> & x_b, double dt,
     Eigen::Matrix<double, 6, 1> & delta_x) override;
 
 private:
--- kinematics_interface_kdl/src/kinematics_interface_kdl.cpp
+++ kinematics_interface_kdl/src/kinematics_interface_kdl.cpp
@@ -242,7 +242,7 @@ bool KinematicsInterfaceKDL::calculate_link_transform(
 }
 
 bool KinematicsInterfaceKDL::calculate_frame_difference(
-  const Eigen::Matrix<double, 7, 1> & x_a, const Eigen::Matrix<double, 7, 1> & x_b, double dt,
+  Eigen::Matrix<double, 7, 1> & x_a, Eigen::Matrix<double, 7, 1> & x_b, double dt,
   Eigen::Matrix<double, 6, 1> & delta_x)
 {
   // verify inputs
